c2c2963e00c00752fcf0960db4425991fc266e4e
Christoph Hafemeister <chafemeister@nygenome.org>
2018-01-17 00:17:10 +0100

    allow passing file paths for loading and saving data as command line arguments (previous default values remain unchanged)

--- src/tsne.h
+++ src/tsne.h
@@ -44,7 +44,7 @@ public:
     int run(double* X, int N, int D, double* Y, int no_dims, double perplexity, double theta, int rand_seed,
              bool skip_random_init, int max_iter, int stop_lying_iter, int mom_switch_iter, int K, double sigma , int nbody_algo, int knn_algo, double early_exag_coeff,double * initialError, double* costs, bool no_momentum_during_exag,
 		int start_late_exag_iter, double late_exag_coeff, int n_trees,int search_k,int nterms, double intervals_per_integer, int min_num_intervals);
-    bool load_data(double** data, int* n, int* d, int* no_dims, double* theta,
+    bool load_data(const char *data_path, double** data, int* n, int* d, int* no_dims, double* theta,
 		    double* perplexity, int* rand_seed, int* max_iter, int*
 		    stop_lying_iter, int* K, double * sigma, int* nbody_algo,
 		    int* compexag, double* early_exag_coeff,  int *
@@ -53,7 +53,7 @@ public:
 	       	    int * nterms, double * intervals_per_integer, int *min_num_intervals
 		    );
     //bool load_initial_data(double** data);
-    void save_data(double* data, int* landmarks, double* costs, int n, int d, double initialError);
+    void save_data(const char *result_path, double* data, int* landmarks, double* costs, int n, int d, double initialError);
     void symmetrizeMatrix(unsigned int** row_P, unsigned int** col_P, double** val_P, int N); // should be static!
 
 
