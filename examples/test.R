83e89e7b07033f657424cb69426e22f2747d9ff2
rec3141 <rec3141@gmail.com>
2018-07-02 15:56:22 -0800

    fixed broken plot

--- examples/test.R
+++ examples/test.R
@@ -14,10 +14,10 @@ d <- 3;
 input_data <- rbind(mvrnorm(n = N/2, rep(0, d), diag(d)),
 	      mvrnorm(n = N/2, rep(100, d), diag(d))
 	      )
-Y2 <- fftRtsne(input_data,1,max_iter = 400, start_late_exag_iter=300, late_exag_coeff=10);
-plot(Y2[,1],runif(length(Y2)),col=c(rep(1,N/2), rep(2,N/2))) # Plot the result
+Y2 <- fftRtsne(input_data,d,max_iter = 400, start_late_exag_iter=300, late_exag_coeff=10);
+#plot(Y2[,1],runif(length(Y2)),col=c(rep(1,N/2), rep(2,N/2))) # Plot the result
 
 
 
-Y2 <- (matrix(Y, ncol=d));
-plot(Y2[,1],Y2[,2],col=c(rep(1,N/2), rep(2,N/2))) # Plot the result
+Y2 <- (matrix(Y2, ncol=d));
+plot(Y2[,1],Y2[,2],col=c(rep(1,N/d), rep(2,N/d))) # Plot the result
