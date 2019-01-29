ae6969bf2daf4e1e56dd9b1a3e3dda4df0af9f91
Alec Hoyland <entropyvsenergy@posteo.de>
2019-01-29 11:49:48 -0500

    Allow compiling on Windows

--- fast_tsne.m
+++ fast_tsne.m
@@ -283,7 +283,7 @@ function [mappedX, costs, initialError] = fast_tsne(X, opts)
     
     % Compile t-SNE C code
     if(~exist(fullfile(tsne_path,'./fast_tsne'),'file') && isunix)
-        system(sprintf('g++ -std=c++11 -O3  src/sptree.cpp src/tsne.cpp src/nbodyfft.cpp  -o fast_tsne -pthread -lfftw3 -lm'));
+ 	system(sprintf('g++ -std=c++11 -O3  src/sptree.cpp src/tsne.cpp src/nbodyfft.cpp  -o bin/fast_tsne -pthread -lfftw3 -lm'));    end
     end
     
     % Compile t-SNE C code on Windows
